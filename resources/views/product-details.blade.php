574ac6b13118a812ca81d72120f6b319ffeb1c7a
amey <Amay>
2017-05-02 11:32:01 +0530

    new

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -185,11 +185,12 @@
                 <table class="table table-bordered">
 				<tbody>
 				<tr class="techSpecRow"><th colspan="2">Product Details</th></tr>
-				<tr class="techSpecRow"><td class="techSpecTD1">Brand: </td><td class="techSpecTD2">Fujifilm</td></tr>
-				<tr class="techSpecRow"><td class="techSpecTD1">Model:</td><td class="techSpecTD2">FinePix S2950HD</td></tr>
-				<tr class="techSpecRow"><td class="techSpecTD1">Released on:</td><td class="techSpecTD2"> 2011-01-28</td></tr>
-				<tr class="techSpecRow"><td class="techSpecTD1">Dimensions:</td><td class="techSpecTD2"> 5.50" h x 5.50" w x 2.00" l, .75 pounds</td></tr>
-				<tr class="techSpecRow"><td class="techSpecTD1">Display size:</td><td class="techSpecTD2">3</td></tr>
+				<tr class="techSpecRow"><td class="techSpecTD1">Brand: </td><td class="techSpecTD2">{{$data['subcategory']}}</td></tr>
+				<tr class="techSpecRow"><td class="techSpecTD1">Model:</td><td class="techSpecTD2">{{$data['name']}}</td></tr>
+				<tr class="techSpecRow"><td class="techSpecTD1">Released on:</td><td class="techSpecTD2"> Released 2017, March
+</td></tr>
+				<tr class="techSpecRow"><td class="techSpecTD1">Dimensions:</td><td class="techSpecTD2"> 145g, 9.5mm thickness</td></tr>
+				<tr class="techSpecRow"><td class="techSpecTD1">Display size:</td><td class="techSpecTD2">5"</td></tr>
 				</tbody>
 				</table>
 				
